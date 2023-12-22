85471bf597aac789ab23906259d052c0c2240ab9
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-22 23:52:10 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -1,11 +1,7 @@
 package com.webapp.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.*;
+
 
 @Entity
 @Table(name="registrations")
