5eb4866a6ecb386aec3f68aaabb76e4a226ed8d6
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-27 23:53:01 +0530

    RestRegistrationController

---
+++ RestRegistrationController.java
@@ -0,0 +1,26 @@
+package com.webapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webapp.entity.Registration;
+import com.webapp.repository.RegistrationRepository;
+
+@RestController
+@RequestMapping("/api/registrations")
+public class RestRegistrationController {
+	
+	@Autowired
+	private RegistrationRepository registrationRepository;
+	
+	@GetMapping
+public List<Registration> getAllReg(){
+	List<Registration>registrations = registrationRepository.findAll();
+	return registrations;
+	
+}
+}
