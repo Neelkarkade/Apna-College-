3854ccd0f7aa9b1486db471e39afb23c4f679658
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-29 23:55:13 +0530

    RestRegistrationController

--- RestRegistrationController.java
+++ RestRegistrationController.java
@@ -2,11 +2,24 @@ package com.webapp.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.webapp.dto.ReadRegistrationDto;
+import com.webapp.dto.RegistrationDto;
 import com.webapp.entity.Registration;
 import com.webapp.repository.RegistrationRepository;
 
@@ -18,9 +31,49 @@ public class RestRegistrationController {
 	private RegistrationRepository registrationRepository;
 	
 	@GetMapping
-public List<Registration> getAllReg(){
+public ResponseEntity<ReadRegistrationDto>getAllReg(){
 	List<Registration>registrations = registrationRepository.findAll();
-	return registrations;
+	ReadRegistrationDto dto = new ReadRegistrationDto();
+	dto.setRegistrations(registrations);
+	dto.setMessage("Reading Completed!!!");
+	return new ResponseEntity<>(dto, HttpStatus.OK);
 	
 }
+	
+	//http://localhost:8080/api/registrations/9
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteRegistration(@PathVariable long id) {
+		registrationRepository.deleteById(id);
+		return new ResponseEntity<>("Record is deleted",HttpStatus.OK);
+	}
+	//Save Registration
+	
+	
+	//http://localhost:8080/api/registrations
+	@PostMapping
+	public ResponseEntity<?> saveRegistration( @Valid @RequestBody Registration reg, BindingResult result) {
+	if (result.hasErrors()) {
+		return new ResponseEntity<>(result.getFieldError().getDefaultMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+		Registration savedReg = registrationRepository.save(reg);
+		RegistrationDto dto = new RegistrationDto();
+		dto.setFirstName(savedReg.getFirstName());
+		dto.setEmail(savedReg.getEmail());
+		dto.setMobile(savedReg.getMobile());
+		return new ResponseEntity(dto, HttpStatus.CREATED);
+	}
+	//http://localhost:8080/api/registrations?id=10
+//	@PutMapping
+//	public ResponseEntity<Registration> updateRegistration(@RequestParam long id,@RequestBody RegistrationDto registrationDto) {
+//		
+//		
+//		Registration registration = registrationRepository.findById(id).get();
+//	registration.setFirstName(registrationDto.getFirstName());
+//	registration.setLastName(registrationDto.getLastName());
+//	registration.setEmail(registrationDto.getEmail());
+//	registration.setMobile(registrationDto.getMobile());
+	
+//	Registration updateReg = registrationRepository.save(registration);
+//	return new ResponseEntity<>(updateReg, HttpStatus.OK);
+///	}
 }
