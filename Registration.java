7058466400136ca70dcfd0110d1a49d1a9d37a24
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2023-12-30 23:02:04 +0530

    Registration

--- Registration.java
+++ Registration.java
@@ -1,62 +1,64 @@
 package com.webapp.entity;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
-
 
 @Entity
-@Table(name="registrations")
+@Table(name="registration")
 public class Registration {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-
-private long id;
-	@NotEmpty
-	@Size(min = 2,message = "minmumm 2 charcter")
-@Column(name ="first_name",length = 45)
-private String firstName;//first_name
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	
-@Column(name ="lastName",length = 45)
-private String lastName;
-@Email
-@Column(name ="email",unique = true,  length = 128)
-private String email;
+	@Column(name ="name",length = 45)
+	private String name;
+		
+	@Column(name ="city",length = 45)
+	private String city;
 
-@Column(name = "mobile")
-private long mobile;
+	@Column(name ="email",unique = true,  length = 128)
+	private String email;
 
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public long getMobile() {
-	return mobile;
-}
-public void setMobile(long mobile) {
-	this.mobile = mobile;
-}
+	@Column(name = "mobile")
+	private long mobile;
 
+	public long getId() {
+		return id;
+	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+   
 }
