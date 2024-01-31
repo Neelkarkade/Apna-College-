953b45ade97f57940f670dd703610cc28f7b89cb
Neelkarkade <78798304+Neelkarkade@users.noreply.github.com>
2024-01-31 18:35:32 +0530

    A

--- A.java
+++ A.java
@@ -1,6 +1,25 @@
-package p1;
-class A1 {
-	  public void test1() {
-		  System.out.println(500);
-	  }
+import java.sql.*;
+
+public class A {
+	public static void main(String[] args) {
+		
+		//Connect to database
+		try {
+			
+
+			//Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/neel_demo_db", "root", "test");
+			System.out.println(con);
+			
+			
+		} catch (Exception e) {
+			
+		}
+		
+		//perform sql Operation
+		
+		//close the connecction
+		
 	}
+
+}
